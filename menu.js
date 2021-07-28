14e75f776060b61c3a633a1a8449ec7dce475356
Ana <87034882+openthehappiness@users.noreply.github.com>
2021-07-28 16:00:15 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,27 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  }
+
+  addClickEvent() {
+      this.mobileMenu.addEventListener("click", () => console.log ("Hey "));
+  } 
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }  
+    return this;
+  }
+}
+
+const MobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+MobileNavbar.init();
+
